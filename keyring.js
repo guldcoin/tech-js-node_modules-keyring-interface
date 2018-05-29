5e93918213cd6a6a46b04ab772bf89d3d98e83d6
Ira Miller <public@iramiller.com>
2018-05-29 05:36:47 -0500

    finish porting from app

--- keyring.js
+++ keyring.js
@@ -1,37 +1,78 @@
-module.exports = {
-  get pubkey () {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  get isLocked () {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  generate() {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  async unlock (pass) {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  async lock () {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  async sign (message) {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  async verify (message, signature) {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  async decrypt (message, keys) {
-    throw new ReferenceError('Function not implemented')
-  }
-
-  async encrypt (message, keys) {
-    throw new ReferenceError('Function not implemented')
-  }
+async function generate (options) {
+  throw new ReferenceError('Function not implemented')
 }
+
+async function clear () {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function store () {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function listKeys () {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function importPublicKey (key) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function importPrivateKey (key) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function getPublicKey (fpr) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function getPrivateKey (fpr) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function isLocked (fpr) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function unlockKey (fpr, pass) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function lockKey (fpr, pass) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function sign (message, fpr) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function verify (message, signature, signers) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function decrypt (message, fpr) {
+  throw new ReferenceError('Function not implemented')
+}
+
+async function encrypt (message, fpr) {
+  throw new ReferenceError('Function not implemented')
+}
+
+module.exports = {
+  generate: generate,
+  clear: clear,
+  store: store,
+  listKeys: listKeys,
+  importPublicKey: importPublicKey,
+  importPrivateKey: importPrivateKey,
+  getPublicKey: getPublicKey,
+  getPrivateKey: getPrivateKey,
+  isLocked: isLocked,
+  unlock: unlock,
+  lock: lock,
+  sign: sign,
+  verify: verify,
+  decrypt: decrypt,
+  encrypt: encrypt
+}
+
